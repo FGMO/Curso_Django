df69d74db11f48ae49d7ba5b58add40b65b4b742
FGMO <fmontalvo8a@hotmail.com>
2019-03-14 23:55:15 -0500

    Creacion del perfil de usuario.

---
+++ usuarios/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class Profile(models.Model):
+    
+
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    website = models.URLField(max_length=200, blank=True)
+    biografia = models.TextField(blank=True)
+    telefono = models.CharField(max_length=12, blank=True)
+    imagen = models.ImageField(upload_to='usuarios/imagenes', blank=True, null=True)
+    adicionado = models.DateField(auto_now_add=True)
+    modificado = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.usuario.username
