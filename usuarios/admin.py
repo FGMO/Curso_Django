680c98a7d656ddc7fe6cfb63a52314a1bb40ab02
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2019-03-25 23:07:45 -0500

    Manipulacion del perfil a traves del dashboard.

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -7,4 +7,8 @@ from usuarios.models import Profile
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     
-    list_display = ('usuario', 'telefono', 'website', 'imagen')
+    list_display = ('id', 'usuario', 'telefono', 'website', 'imagen')
+    list_display_links = ('id', 'usuario')
+    list_editable = ('website', 'telefono', 'imagen')
+    search_fields = ('usuario__id', 'usuario__username', 'usuario__email')
+    list_filter = ('usuario__is_active', 'adicionado', 'modificado')
